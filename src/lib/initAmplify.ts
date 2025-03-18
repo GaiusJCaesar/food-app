4db830b5b03eaeb87c5b53fe184142986ca745bd
GaiusJCaesar <lewisdean2010@gmail.com>
2025-03-18 14:28:49 +0000

    add api gw

--- src/lib/initAmplify.ts
+++ src/lib/initAmplify.ts
@@ -1,15 +1,22 @@
 import { Amplify } from "aws-amplify";
 import outputs from "../../amplify_outputs.json";
+import { parseAmplifyConfig } from "aws-amplify/utils";
+
+const amplifyConfig = parseAmplifyConfig(outputs);
 
 Amplify.configure(
   {
-    ...outputs,
+    ...amplifyConfig,
     Auth: {
-      ...outputs.auth,
+      ...amplifyConfig.Auth,
       Cognito: {
         userPoolClientId: outputs.auth.user_pool_client_id,
         userPoolId: outputs.auth.user_pool_id,
       },
+    },
+    API: {
+      ...amplifyConfig.API,
+      REST: outputs.custom.API,
     },
   },
   { ssr: false }
