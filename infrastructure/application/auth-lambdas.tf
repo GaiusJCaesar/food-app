ff095822a408f864dff5e68b59cf18aad6307f16
GaiusJCaesar <lewisdean2010@gmail.com>
2025-03-19 17:50:09 +0000

    remove event source mapping

--- infrastructure/application/auth-lambdas.tf
+++ infrastructure/application/auth-lambdas.tf
@@ -12,16 +12,10 @@ resource "aws_lambda_function" "auth_lambda" {
   runtime       = "nodejs20.x"
 }
 
-resource "aws_lambda_event_source_mapping" "auth_lambda" {
-  event_source_arn = aws_cognito_user_pool.default_pool.arn
-  function_name    = aws_lambda_function.auth_lambda.arn
-}
-
 resource "aws_lambda_permission" "auth_lambda" {
   statement_id  = "AllowExecutionFromCognito"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.auth_lambda.function_name
   principal     = "cognito-idp.amazonaws.com"
   source_arn    = aws_cognito_user_pool.default_pool.arn
-  depends_on    = [aws_lambda_function.auth_lambda]
 }
