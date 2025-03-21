ec9fea252257db948f2b5999a42e49f80b230aeb
GaiusJCaesar <lewisdean2010@gmail.com>
2025-03-21 10:01:15 +0000

    Update api.yml

--- src/components/providers/auth-provider.tsx
+++ src/components/providers/auth-provider.tsx
@@ -7,7 +7,7 @@ const cognitoAuthConfig = {
   client_id: "4s82t34ei1j3da5c5nid1dn547",
   redirect_uri: "http://localhost:3000/",
   response_type: "code",
-  scope: "email openid",
+  scope: "email openid urn:food-app:resource-server/api-gw-prod",
 };
 
 function AuthProvider({ children }: PropsWithChildren) {
