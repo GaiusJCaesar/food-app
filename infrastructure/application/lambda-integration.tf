a0b623d221d8457a0547d5953dbf2628240bc2e7
GaiusJCaesar <lewisdean2010@gmail.com>
2025-06-14 18:12:18 +0100

    fix new module pt2

--- infrastructure/application/lambda-integration.tf
+++ infrastructure/application/lambda-integration.tf
@@ -1,12 +1,18 @@
 module "users_routes" {
   source = "../modules/api_lambda_integration"
 
-  api_id              = aws_apigatewayv2_api.shared_api.id
+  api_id               = aws_apigatewayv2_api.shared_api.id
   lambda_function_name = aws_lambda_function.user_lambda.function_name
   lambda_invoke_arn    = aws_lambda_function.user_lambda.invoke_arn
 
   routes = [
-    { path = "/users",    methods = ["POST"] },
-    { path = "/users/{id}", methods = ["GET", "PUT", "DELETE"] }
+    {
+      path    = "/users"
+      methods = ["POST"]  # Single POST method on /users path
+    },
+    {
+      path    = "/users/{id}"
+      methods = ["GET", "PUT", "DELETE"]  # Multiple methods on /users/{id}
+    }
   ]
 }
